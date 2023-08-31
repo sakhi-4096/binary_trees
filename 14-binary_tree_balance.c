d29df6414d643a833dc0d71fb4fa1fbe57e120bc
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-08-31 23:22:06 +0200

    Update helper function.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,24 +6,18 @@
  *
  * Return: Balanced height of the tree, 0 if tree is NULL
  */
-size_t binary_tree_height_b(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0;
 	size_t right_height = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	else
-	{
-		if (tree)
-		{
-			left_height = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
-			right_height = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
-		}
-		return ((left_height > right_height) ? left_height : right_height);
-	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
@@ -38,8 +32,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		left_height = (int)binary_tree_height_b(tree->left);
-		right_height = (int)binary_tree_height_b(tree->right);
+		left_height = (int)binary_tree_height(tree->left);
+		right_height = (int)binary_tree_height(tree->right);
 		balance = left_height - right_height;
 	}
 	return (balance);
