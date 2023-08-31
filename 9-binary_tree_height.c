dcf6093bbec69cf2aaa0b9bafcc38518596254c7
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-08-31 22:35:13 +0200

    Fix segmentation fault.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,17 +2,23 @@
 
 /**
  * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root of the tree to measure tje height
+ * @tree: Pointer to the root node of the tree to measure the height
  *
- * Return: Height of the tree, 0 if the tree is NULL
+ * Return: Height of the tree, 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return ((left_height > right_height) ? left_height : right_height);
 }
