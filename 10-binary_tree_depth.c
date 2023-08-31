e6acfdad6620fdc2fae904973170938b0cd32dda
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-08-31 22:39:10 +0200

    Fix output on more test cases.

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,13 +8,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
-
-	while (tree != NULL)
-	{
-		tree = tree->parent;
-		depth++;
-	}
-
-	return (depth);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
